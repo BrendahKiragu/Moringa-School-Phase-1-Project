d26c9df7ba9a855e97032af76a0258b5019825cc
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-16 19:30:37 +0300

    creates a function to display fecthed books

--- script.js
+++ script.js
@@ -24,6 +24,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const suggestionsLink = document.getElementById('suggestions-link');
   const suggestionsSection = document.getElementById('suggestions');
 
+//this is a container for the fetched books
+function displayBooks(books){
+const bookDisplay = document.getElementById('book-display');
+bookDisplay.innerHTML = '';
+
+books.forEach(book => {
+const bookCard = document.createElement('div'); //creates a new div for each book
+bookCard.classList.add('book');//for css styling
+//content for each bookCard: img,title,author name
+//unknown rep when author name is unknown
+//@ div is appended to the div container of book collection
+bookCard.innerHTML = `
+  <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" alt="${book.title}">
+  <div class="book-details">
+    <h3>${book.title}</h3>
+    <p>by ${book.author_name ? book.author_name.join(', ') : 'Unknown'}</p>
+  </div>
+`;
+bookDisplay.appendChild(bookCard);
+})
+}
+
+//event listeners
   if (aboutLink && aboutSection) {
     console.log('Elements found');
     aboutLink.addEventListener('click', (event) => {
@@ -42,6 +65,9 @@ document.addEventListener('DOMContentLoaded', () => {
     suggestionsLink.addEventListener('click', (event) => {
       event.preventDefault();
       suggestionsSection.classList.toggle('hidden');
+
+      fetchBooks('query')
     });
   }
 });
+
