5e3407edbfc24214c69852c2da503a2a9888613c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-16 17:20:15 +0300

    creates an bout section with toggle effect

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 //FETCH API
 // Fetch API Request
 //returned data has an object with a docs array
-function fetchBooks (){
-const url = 'http://localhost:3000/books';
+// function fetchBooks (){
+// const url = 'http://localhost:3000/books';
 
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    // const books = data.slice(0, 10);
-    // displayBooks(books); 
-  })
-  .catch(error => {
-    console.log("Error fetching books:", error);
-  });
-}
+// fetch(url)
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//      const books = data.slice(0, 10);
+//      displayBooks(books); 
+//   })
+//   .catch(error => {
+//     console.log("Error fetching books:", error);
+//   });
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const aboutLink = document.getElementById('about-link');
+  const aboutSection = document.getElementById('about');
+
+  if (aboutLink && aboutSection) {
+    console.log('Elements found');
+    aboutLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      console.log('About link clicked');
+      aboutSection.classList.toggle('hidden');
+    });
+  } else {
+    console.error('Elements not found');
+  }
+});
