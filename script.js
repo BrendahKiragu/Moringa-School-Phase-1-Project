71229bf0b10d6697e1a7e9199fd4084513ce48cb
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-19 16:02:40 +0300

    sets a limit to fetched books

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function fetchBooks(searchTerm){
   fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(searchTerm)}`) 
     .then(res => res.json())
     .then(data => {
-      const books = data.docs
+      const books = data.docs.slice(0, 5)
       displayBooks(books, searchTerm)
     })
     .catch(error=> {console.log('Error fetching books:', error)
